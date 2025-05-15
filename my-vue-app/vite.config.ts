e7f6dac323288cff3b776b7c77f4b4d301596d5d
Artyom <fokeicheff2018@gmail.com>
2025-05-15 19:46:48 +0500

    Fix

--- my-vue-app/vite.config.ts
+++ my-vue-app/vite.config.ts
@@ -1,25 +1,31 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import fs from 'node:fs'
 // https://vite.dev/config/
-export default defineConfig({
-  define: {
-    global: 'globalThis'
-  },
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      global: 'globalThis',
+      ...Object.entries(env).reduce((prev, [key, val]) => ({
+        ...prev,
+        [`process.env.${key}`]: JSON.stringify(val)
+      }), {})
     },
-  },
-  server: {
-    // https: {
-    //   key: fs.readFileSync(path.resolve(__dirname, 'localhost+2-key.pem')),
-    //   cert: fs.readFileSync(path.resolve(__dirname, 'localhost+2.pem')),
-    // },
-    host: '127.0.0.1'
-  },
-  
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      // https: {
+      //   key: fs.readFileSync(path.resolve(__dirname, 'localhost+2-key.pem')),
+      //   cert: fs.readFileSync(path.resolve(__dirname, 'localhost+2.pem')),
+      // },
+      host: '127.0.0.1'
+    },
+  }
 })
